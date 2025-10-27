e235f83bba7c737019d5cd2df69347e1ab88fcb8
Hellorge <kdevmen@gmail.com>
2025-10-27 07:59:45 +0000

    added missing child table in loc

---
+++ import_export/import_export/doctype/lc_required_document/lc_required_document.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, gws and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class LCRequiredDocument(Document):
+	pass
